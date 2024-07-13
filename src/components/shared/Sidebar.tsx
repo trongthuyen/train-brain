f6dce44e40e8929030b249365a376167ded77537
Tee <thuyen.nong-trong@banvien.com.vn>
2024-07-13 17:35:21 +0700

    responsive sidebar mobile

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,10 +1,41 @@
 import logo from "../../assets/logo/TrainBrainLogo.svg";
 import { DASHBOARD_SIDEBAR_LINKS } from "../../lib/constants";
 import SidebarLink from "./SidebarLink";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 
 const Sidebar = () => {
+  const [openSidebarMobile, setOpenSidebarMobile] = useState(false);
+
+  const toggleOpenSidebarMobile = (e: any) => {
+    e?.stopPropagation();
+    setOpenSidebarMobile(!openSidebarMobile);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", () => setOpenSidebarMobile(false));
+    return () => {
+      document.removeEventListener("click", () => setOpenSidebarMobile(false));
+    };
+  }, []);
+
   return (
-    <div className="bg-white shadow-sm w-fit md:w-60 px-1 py-3 md:px-3 flex flex-col text-gray-700 rounded-tr-[20px] rounded-br-[20px]">
+    <div
+      className={classNames(
+        openSidebarMobile ? "left-0" : "-left-full",
+        "absolute top-0 bottom-0 duration-300 z-[100] sm:relative sm:left-0 bg-white shadow-sm w-fit md:w-60 px-1 py-3 md:px-3 flex flex-col text-gray-700 rounded-tr-[20px] rounded-br-[20px]"
+      )}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div
+        className={classNames(
+          openSidebarMobile && "opacity-0 -translate-x-full",
+          "duration-300 sm:hidden z-[100] bg-white rounded-xl p-3 fixed top-14 left-0 -translate-x-1/4 shadow-lg cursor-pointer"
+        )}
+        onClick={toggleOpenSidebarMobile}
+      >
+        <img src={logo} alt="" />
+      </div>
       <div className="flex items-center justify-center md:justify-normal gap-2 px-0 xs:px-3 md:px-6 select-none cursor-pointer">
         <img src={logo} alt="" className="min-w-10" />
         <h3 className="text-black text-xl font-bold hidden md:block">
